1f76f1f526c067de3321bffabfe2450c2952525f
Hossam TAROUCHI <hossam@ikdev.fr>
2019-07-31 23:37:05 +0100

    Handling errors and unprogrammed routes

--- app.js
+++ app.js
@@ -13,4 +13,18 @@ app.use(morgan('dev'));
 
 app.use('/spends', spendsRoutes);
 
+app.use((req, res, next) => {
+    const error = new Error("Not Found");
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500).json({
+        error:{
+            message: error.message
+        }
+    });
+});
+
 module.exports = app;
